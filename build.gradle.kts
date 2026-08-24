6b87019206308178bef5d2f6a91abe463566da70
ThanhMiLa <vntvlogs@gmail.com>
2026-08-25 00:01:41 +0700

    Update project configuration and plugin metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,28 @@
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
+    id("java")
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.intellij.platform")
     id("org.jetbrains.changelog")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+
     testImplementation("junit:junit:4.13.2")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         intellijIdea("2025.2.6.2")
+        bundledPlugin("com.intellij.java")
         testFramework(TestFrameworkType.Platform)
     }
 }
